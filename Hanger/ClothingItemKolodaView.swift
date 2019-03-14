3834df12e16e0191f994d5a79ad35dabde6e5683
Naman Kedia <naman@Namans-MacBook-Pro.local>
2019-03-14 18:26:08 -0400

    first commit

---
+++ Hanger/ClothingItemKolodaView.swift
@@ -0,0 +1,21 @@
+//
+//  ClothingItemKolodaView.swift
+//  Hanger
+//
+//  Created by Naman Kedia on 3/14/19.
+//  Copyright © 2019 Naman Kedia. All rights reserved.
+//
+
+import UIKit
+
+class ClothingItemKolodaView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
